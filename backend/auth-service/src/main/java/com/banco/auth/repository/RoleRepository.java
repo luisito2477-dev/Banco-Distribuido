9ed5544923e8c9999f74e1943e7a8c443f95f2ec
luisito2477-dev <lfhp2477@gmail.com>
2026-07-09 19:33:34 -0600

    Creando los repositorios en el microservicio auth

--- backend/auth-service/src/main/java/com/banco/auth/repository/RoleRepository.java
+++ backend/auth-service/src/main/java/com/banco/auth/repository/RoleRepository.java
@@ -4,26 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 import java.util.Optional;
+import com.banco.auth.entity.Role;
 
-import com.banco.auth.entity.User;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
     /**
      * Metodos ya incluidos por JpaRepository
-     * save(User user) (Sirve tanto para registrar nuevos como para actualizar).
+     * save(Role role) (Sirve tanto para registrar nuevos como para actualizar).
      *
      * findById(UUID id) (Busca por llave primaria).
      *
-     * findAll() (Trae a todos los usuarios).
+     * findAll() (Trae a todos los roles).
      *
      * deleteById(UUID id) (Borra de la base de datos).
      *
      * existsById(UUID id) (Verifica si el ID ya existe).
      */
 
-    Optional<User> findByEmail(String email);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByCurp(String curp);
+    Optional<Role> findByRoleName(String roleName);
 }
